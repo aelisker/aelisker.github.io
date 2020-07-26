3d551c9cf7a2a898cbf1f32769ff2e0705e26444
Aaron Lisker <aelisker@gmail.com>
2020-07-25 20:34:27 -0700

    added json file of projects

--- src/components/pages/work.js
+++ src/components/pages/work.js
@@ -3,20 +3,59 @@ import React from 'react';
 function Work() {
   return (
     <section id="work" className="about">
-      <div className="flex-row">
-        <div className="section-title">
+      <div class="flex-row">
+        <div class="section-title">
           <h2>
-            Work - Needs Changing
+            My Work
           </h2>
         </div>
-        <div className="content flex-row">
-          <p>
-            butcher selfies chambray shabby chic gentrify readymade yr Echo Park XOXO Tumblr normcore Banksy direct trade Blue Bottle chillwave you probably haven't heard of them single-origin coffee Vice fanny pack fixie Odd Future Austin fingerstache pickled twee synth Wes Anderson Thundercats viral bitters flannel meggings narwhal Marfa Schlitz sustainable Intelligentsia umami deep v craft
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, minima molestiae vero eum magni dignissimos voluptates aliquam nisi dolores eos autem eveniet repellat natus corporis, magnam voluptatem pariatur possimus enim!
-          </p>
+        <div class="content service-grid-wrapper">
+          <div class="service-grid-container">
+                        <div class="service-grid-item panel-1">
+              <a href="https://tech-stop-n-shop.herokuapp.com/">
+                <div class="grid-text active">
+                  <h3>Tech-Stop-N-Shop</h3>
+                  <h4>Node MVC Application</h4>
+                </div>
+              </a>
+            </div>
+            <div class="service-grid-item panel-2">
+              <a href="https://github.com/aelisker/netflix-movie-picker">
+                <div class="grid-text active">
+                  <h3>Netflix Movie Picker</h3>
+                  <h4>JS w/ API calls, Foundation</h4>
+                </div>
+              </a>
+            </div>
+            <div class="service-grid-item panel-3">
+              <a href="https://aelisker.github.io/weather-dashboard/">
+                <div class="grid-text active">
+                  <h3>Weather Dashboard</h3>
+                  <h4>JS w/ API call</h4>
+                </div>
+              </a>
+            </div>
+            <div class="service-grid-item panel-4">
+              <a href="https://aelisker.github.io/workday-scheduler/">
+                <div class="grid-text active">
+                  <h3>Workday Scheduler</h3>
+                  <h4>JS w/ Bootstrap</h4>
+                </div>
+              </a>
+            </div>
+            <div class="service-grid-item panel-5">
+              <a href="https://aelisker.github.io/coding-quiz/">
+                <div class="grid-text active">
+                  <h3>Javascript Quiz</h3>
+                  <h4>JS w/ Localstorage</h4>
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
+
   )
 }
 
